ec871a2498b3e001f3ed633e05d2cb2b08960fb5
DustinSun <duongnvh.work@gmail.com>
2024-05-19 01:09:37 +0700

    ...

--- mosaic_subnet/miner/model.py
+++ mosaic_subnet/miner/model.py
@@ -11,6 +11,12 @@ from communex.module.module import Module, endpoint
 
 from huggingface_hub import hf_hub_download
 
+from transformers import pipeline, set_seed
+
+import re
+
+import random
+
 base_model_id = "stabilityai/stable-diffusion-xl-base-1.0"
 repo_name = "ByteDance/Hyper-SD"
 ckpt_name = "Hyper-SDXL-8steps-CFG-lora.safetensors"
@@ -28,7 +34,28 @@ class DiffUsers(Module):
         self.pipeline.scheduler = DDIMScheduler.from_config(self.pipeline.scheduler.config, timestep_spacing="trailing")
         self.steps = 8
 
+        # prompt
+        self.magic_prompt_pipe = pipeline('text-generation', model='Gustavosta/MagicPrompt-Stable-Diffusion', tokenizer='gpt2')
+
         self._lock = threading.Lock()
+
+    def magic_prompt(self, raw_prompt: str) -> str:
+        prompt = raw_prompt.replace("\n", "").lower().capitalize()
+        prompt = re.sub(r"[,:\-–.!;?_]", '', prompt)
+        generated_text = prompt
+
+        for count in range(5):
+            seed = random.randint(100, 1000000)
+            set_seed(seed)
+
+            response = self.magic_prompt_pipe(prompt, pad_token_id=50256, max_length=77, truncation=True)[0]
+            generated_text = response['generated_text'].strip()
+            if len(generated_text) > (len(prompt) + 8):
+                if generated_text.endswith((":", "-", "—")) is False:
+                    generated_text = re.sub(r'[^ ]+\.[^ ]+', '', generated_text).replace("<", "").replace(">", "")
+                break
+
+        return generated_text
 
     @endpoint
     def sample(
@@ -38,9 +65,10 @@ class DiffUsers(Module):
         if seed is None:
             seed = generator.seed()
         generator = generator.manual_seed(seed)
+        magic_prompt = self.magic_prompt(prompt)
         with self._lock:
             image = self.pipeline(
-                prompt=prompt,
+                prompt=magic_prompt,
                 negative_prompt=negative_prompt,
                 num_inference_steps=self.steps,
                 generator=generator,
