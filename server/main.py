38d416166c22b1ceb11aa9078cc8f3d723d29924
DustinSun <duongnvh.work@gmail.com>
2024-05-19 19:56:26 +0700

    ...

--- server/main.py
+++ server/main.py
@@ -55,7 +55,7 @@ class DiffUsers:
                 negative_prompt=negative_prompt,
                 num_inference_steps=self.steps,
                 generator=generator,
-                guidance_scale=2.0
+                guidance_scale=8
             ).images[0]
         buf = BytesIO()
         image.save(buf, format="png")
