c175a38524fd0b0ed0532fbc6fe1166b2dbf7922
DustinSun <duongnvh.work@gmail.com>
2024-05-19 15:58:20 +0700

    ...

--- server/main.py
+++ server/main.py
@@ -1,5 +1,5 @@
-
-from io import BytesIO
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
 from typing import Optional
 import base64
 import threading
@@ -13,6 +13,8 @@ from huggingface_hub import hf_hub_download
 
 from safetensors.torch import load_file
 
+from io import BytesIO
+
 # base_model_id = "stabilityai/stable-diffusion-xl-base-1.0"
 # repo_name = "ByteDance/Hyper-SD"
 # ckpt_name = "Hyper-SDXL-8steps-CFG-lora.safetensors"
@@ -21,17 +23,24 @@ base = "stabilityai/stable-diffusion-xl-base-1.0"
 repo = "ByteDance/SDXL-Lightning"
 ckpt = "sdxl_lightning_8step_unet.safetensors"
 
-class DiffUsers(Module):
-    def __init__(self, model_name: str = "stabilityai/sdxl-turbo") -> None:
-        super().__init__()
-        self.model_name = model_name
+class SampleInput(BaseModel):
+    prompt: str
+    steps: Optional[int] = 2
+    negative_prompt: Optional[str] = ""
+    seed: Optional[int] = None
+
+class DiffUsers:
+    def __init__(self):
+
+        print("setting up model")
         self.device = torch.device("cuda" if torch.cuda.is_available() else "mps")
 
-        # ## n step lora
+        ## n step lora
         # self.pipeline = DiffusionPipeline.from_pretrained(base_model_id, torch_dtype=torch.float16, variant="fp16").to(self.device)
         # self.pipeline.load_lora_weights(hf_hub_download(repo_name, ckpt_name))
         # self.pipeline.fuse_lora()
         # self.pipeline.scheduler = DDIMScheduler.from_config(self.pipeline.scheduler.config, timestep_spacing="trailing")
+        self.steps = 8
 
         self.unet = UNet2DConditionModel.from_config(base, subfolder="unet").to("cuda", torch.float16)
         self.unet.load_state_dict(load_file(hf_hub_download(repo, ckpt), device="cuda"))
@@ -39,15 +48,15 @@ class DiffUsers(Module):
         self.pipeline = StableDiffusionXLPipeline.from_pretrained(base, unet=self.unet, torch_dtype=torch.float16, variant="fp16").to("cuda")
         self.pipeline.scheduler = EulerDiscreteScheduler.from_config(self.pipeline.scheduler.config, timestep_spacing="trailing")
 
-        self.steps = 8
-
-
         self._lock = threading.Lock()
+        print("model setup done")
 
-    @endpoint
-    def sample(
-        self, prompt: str, steps: int = 2, negative_prompt: str = "", seed:
-    Optional[int]=None) -> str:
+    def sample(self, input: SampleInput):
+        prompt = input.prompt
+        # steps = input.steps
+        negative_prompt = input.negative_prompt
+        seed = input.seed
+
         generator = torch.Generator(self.device)
         if seed is None:
             seed = generator.seed()
@@ -58,20 +67,23 @@ class DiffUsers(Module):
                 negative_prompt=negative_prompt,
                 num_inference_steps=self.steps,
                 generator=generator,
-                # guidance_scale=5.0
                 guidance_scale=0.0
             ).images[0]
         buf = BytesIO()
         image.save(buf, format="png")
         buf.seek(0)
-        return base64.b64encode(buf.read()).decode()
+        image = base64.b64encode(buf.read()).decode()
+        return {"image": image}
 
-    @endpoint
-    def get_metadata(self) -> dict:
-        return {"model": self.model_name}
+
+app = FastAPI()
+diffusers = DiffUsers()
+
+
+@app.post("/sample")
+def sample(input: SampleInput):
+    return diffusers.sample(input)
 
 if __name__ == "__main__":
-    d = DiffUsers()
-    out = d.sample(prompt="cat, jumping")
-    with open("a.png", "wb") as f:
-        f.write(out)
+  import uvicorn
+  uvicorn.run(app=app, host="0.0.0.0", port=8000)
