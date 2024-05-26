798858f93d6fdf2e03487ac2a6142c6f64668596
DustinSun <duongnvh.work@gmail.com>
2024-05-26 23:25:17 +0700

    ...

--- server/main.py
+++ server/main.py
@@ -14,9 +14,9 @@ from huggingface_hub import hf_hub_download
 
 from io import BytesIO
 
-base_model_id = "stabilityai/stable-diffusion-xl-base-1.0"
-repo_name = "ByteDance/Hyper-SD"
-ckpt_name = "Hyper-SDXL-8steps-CFG-lora.safetensors"
+# base_model_id = "stabilityai/stable-diffusion-xl-base-1.0"
+# repo_name = "ByteDance/Hyper-SD"
+# ckpt_name = "Hyper-SDXL-8steps-CFG-lora.safetensors"
 
 class SampleInput(BaseModel):
     prompt: str
@@ -30,17 +30,17 @@ class DiffUsers:
         print("setting up model")
         self.device = torch.device("cuda" if torch.cuda.is_available() else "mps")
 
-        ## n step lora
-        self.pipeline = DiffusionPipeline.from_pretrained(base_model_id, torch_dtype=torch.float16, variant="fp16").to(self.device)
-        self.pipeline.load_lora_weights(hf_hub_download(repo_name, ckpt_name))
-        self.pipeline.fuse_lora()
-        self.pipeline.scheduler = DDIMScheduler.from_config(self.pipeline.scheduler.config, timestep_spacing="trailing")
-        self.steps = 8
-        self.guidance_scale = 5
+        # ## n step lora
+        # self.pipeline = DiffusionPipeline.from_pretrained(base_model_id, torch_dtype=torch.float16, variant="fp16").to(self.device)
+        # self.pipeline.load_lora_weights(hf_hub_download(repo_name, ckpt_name))
+        # self.pipeline.fuse_lora()
+        # self.pipeline.scheduler = DDIMScheduler.from_config(self.pipeline.scheduler.config, timestep_spacing="trailing")
+        # self.steps = 8
+        # self.guidance_scale = 5
 
-        # self.pipeline = DiffusionPipeline.from_pretrained("SG161222/RealVisXL_V4.0_Lightning",torch_dtype=torch.float16, variant="fp16").to(self.device)
-        # self.steps = 4
-        # self.guidance_scale = 2
+        self.pipeline = DiffusionPipeline.from_pretrained("SG161222/RealVisXL_V4.0_Lightning",torch_dtype=torch.float16, variant="fp16").to(self.device)
+        self.steps = 4
+        self.guidance_scale = 2
 
         self._lock = threading.Lock()
         print("model setup done")
